72c197d1f48569c638a471d63003345d83e16a2f
emanuelssj <emanuelolz@yahoo.com.br>
2018-08-03 23:02:21 -0300

    Melhorado o método do pinkyAction para ser utilizado também pelo inky

--- src/agents/ghosts/ghost_agent.hh
+++ src/agents/ghosts/ghost_agent.hh
@@ -35,9 +35,9 @@ public:
                     case CHASE:
                         switch(ghost.typeGhost) {
                             case 1: d = PathMagic::from_to(ghost.pos, s.pacman.pos); break; // Blinky- only chase
-                            case 2: d = PathMagic::from_to(ghost.pos, pinkyAction(s, 4)); break; // Pinky - 4 pos in front of pacman
-                            case 3: if(inkyAction(s) > 0) {// Inky - if blinky on chase ((blinky distance of pacman) * 2) in front of pacman else run away
-                                        d = PathMagic::from_to(ghost.pos, pinkyAction(s, inkyAction(s))); break;
+                            case 2: d = PathMagic::from_to(ghost.pos, pinkyAction(s, 4, ghost_id)); break; // Pinky - 4 pos in front of pacman
+                            case 3: if(inkyAction(s) > 0) {// Inky - if blinky on chase ((blinky distance of pacman) * 2) in front of pacman limit of 8 pos else run away
+                                        d = PathMagic::from_to(ghost.pos, pinkyAction(s, inkyAction(s), ghost_id)); break;
                                     }
                                     d = PathMagic::try_to_avoid(ghost.pos, PathMagic::from_to(ghost.pos, s.pacman.pos)); break;
                             case 4: if(clydeAction(s)) {// run from pacman when close <= 4 positions
@@ -50,14 +50,14 @@ public:
                 }
 
                 //ensure(pr.found, "Path not found!");
-                if (d == Direction::STAY) _debug("No movement for ghost #%d!", ghost_id);
+                if (d == Direction::STAY) _debug("No movement for ghost #%d - ghost_id #%d!", ghost.typeGhost, ghost_id);
                 return d;
             }
         }
         else return ghost.dir;
     }
 
-    inline Position pinkyAction(const State& s, int maxPosC) {
+    inline Position pinkyAction(const State& s, int maxPosC, int ghost_id) {
         Position pacmanPosMatrix;
         Position finalPos = Position(s.pacman.pos.i, s.pacman.pos.j);
         int lastMov = 0;
@@ -100,7 +100,7 @@ public:
                     finalPos = Position(finalPos.i + 1, finalPos.j);
             }
             //Pinky position == Dest position
-            if(finalPos.i == s.ghosts[1].pos.i && finalPos.j == s.ghosts[1].pos.j) {
+            if(finalPos.i == s.ghosts[ghost_id].pos.i && finalPos.j == s.ghosts[ghost_id].pos.j) {
                 finalPos = Position(s.pacman.pos.i, s.pacman.pos.j);
             }
             return finalPos;
@@ -138,7 +138,7 @@ public:
                     finalPos = Position(finalPos.i + 1, finalPos.j);
             }
             //Pinky position == Dest position
-            if(finalPos.i == s.ghosts[1].pos.i && finalPos.j == s.ghosts[1].pos.j) {
+            if(finalPos.i == s.ghosts[ghost_id].pos.i && finalPos.j == s.ghosts[ghost_id].pos.j) {
                 finalPos = Position(s.pacman.pos.i, s.pacman.pos.j);
             }
             return finalPos;
@@ -176,7 +176,7 @@ public:
                     finalPos = Position(finalPos.i + 1, finalPos.j);
             }
             //Pinky position == Dest position
-            if(finalPos.i == s.ghosts[1].pos.i && finalPos.j == s.ghosts[1].pos.j) {
+            if(finalPos.i == s.ghosts[ghost_id].pos.i && finalPos.j == s.ghosts[ghost_id].pos.j) {
                 finalPos = Position(s.pacman.pos.i, s.pacman.pos.j);
             }
             return finalPos;
@@ -214,21 +214,25 @@ public:
                     finalPos = Position(finalPos.i + 1, finalPos.j);
             }
             //Pinky position == Dest position
-            if(finalPos.i == s.ghosts[1].pos.i && finalPos.j == s.ghosts[1].pos.j) {
+            if(finalPos.i == s.ghosts[ghost_id].pos.i && finalPos.j == s.ghosts[ghost_id].pos.j) {
                 finalPos = Position(s.pacman.pos.i, s.pacman.pos.j);
             }       
             return finalPos;
         }
+        if(finalPos.i == s.ghosts[ghost_id].pos.i && finalPos.j == s.ghosts[ghost_id].pos.j) {
+            finalPos = Position(s.pacman.pos.i, s.pacman.pos.j);
+        }
         return finalPos;
     }
         
-
     inline int inkyAction(const State& s) {
         int i, j;
+        int limit = 10;
         (s.pacman.pos.i > s.ghosts[0].pos.i) ? i = s.pacman.pos.i - s.ghosts[0].pos.i : i = s.ghosts[0].pos.i - s.pacman.pos.i; // blinky pos and pacman pos
         (s.pacman.pos.j > s.ghosts[0].pos.j) ? j = s.pacman.pos.j - s.ghosts[0].pos.j : j = s.ghosts[0].pos.j - s.pacman.pos.j;
-        if(s.ghosts[0].behaviour == Ghost_Behaviour::CHASE)
-            return ((i + j) * 2);
+        if(s.ghosts[0].behaviour == Ghost_Behaviour::CHASE) {
+            return ((i + j) * 2) > limit ? limit : ((i + j) * 2);
+        }
         return 0;
     }
 
